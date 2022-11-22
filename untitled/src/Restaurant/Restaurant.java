ce35bd9953ce11959113fea1229f6fec9e7d65dc
Italo <italoterto4@gmail.com>
2022-11-22 15:16:14 +0000

    Restaurant class

--- untitled/src/Restaurant/Restaurant.java
+++ untitled/src/Restaurant/Restaurant.java
@@ -1,11 +1,19 @@
 package Restaurant;
 
+import CLI.LoginManager;
+import People.Chef;
+import People.Customer;
+import People.Waiter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
     private List<Food> menu;
-
+    private ArrayList<Table> tables = new ArrayList<>();
+    private ArrayList<Waiter> waiters = new ArrayList<>();
+    private ArrayList<Chef> chefs = new ArrayList<>();
+    private ArrayList<Customer> customers = new ArrayList<>();
     public Restaurant() {
         menu = new ArrayList<>();
     }
@@ -13,4 +21,19 @@ public class Restaurant {
     public List<Food> getMenu() {
         return menu;
     }
+
+    public void addTable() {
+
+    }
+
+    public void addWaiter(Waiter newWaiter) {
+        waiters.add(newWaiter);
+        //LoginManager.createAcc();
+    }
+
+    /*
+    add array lists from the datastorage
+    - add waiter() which will generate an user + password and pass thru the login manager
+    same for others
+     */
 }
