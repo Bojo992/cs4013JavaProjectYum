1d82b688a45ced69c2339267f2f9a1d94c9f0d7b
zed992 <93447305+zed992@users.noreply.github.com>
2022-11-22 12:36:01 +0000

    Add get for tables, chefs, waiters for specific restaurant.

--- untitled/src/utils/dataStorage.java
+++ untitled/src/utils/dataStorage.java
@@ -9,49 +9,59 @@ import java.util.HashMap;
 public class dataStorage {
     private static HashMap<String, Restaurant> allRestaurants = new HashMap<>();
     private static HashMap<Restaurant, ArrayList<Table>> allTables = new HashMap<>();
-    private static HashMap<Restaurant, Waiter> allWaiters = new HashMap<>(); 
-    private static HashMap<Restaurant, Chef> allChefs = new HashMap<>(); 
-    private static HashMap<Restaurant, Customer> allCustomers = new HashMap<>();
+    private static HashMap<Restaurant, ArrayList<Waiter>> allWaiters = new HashMap<>();
+    private static HashMap<Restaurant, ArrayList<Chef>> allChefs = new HashMap<>();
+    private static HashMap<Restaurant, ArrayList<Customer>> allCustomers = new HashMap<>();
 
     public static HashMap<String, Restaurant> getAllRestaurants() {
         return allRestaurants;
     }
 
     public static void setAllRestaurants(HashMap<String, Restaurant> allRestaurants) {
-        dataStorage.allRestaurants = allRestaurants;
+        allRestaurants = allRestaurants;
     }
 
-    public static HashMap<Restaurant, Table> getAllTables() {
+    public static HashMap<Restaurant, ArrayList<Table>> getAllTables() {
         return allTables;
     }
 
-    public static void setAllTables(HashMap<Restaurant, Table> allTables) {
-        dataStorage.allTables = allTables;
+    public static void setAllTables(HashMap<Restaurant, ArrayList<Table>> allTables) {
+        allTables = allTables;
     }
 
-    public static HashMap<Restaurant, Waiter> getAllWaiters() {
+    public static HashMap<Restaurant, ArrayList<Waiter>> getAllWaiters() {
         return allWaiters;
     }
 
-    public static void setAllWaiters(HashMap<Restaurant, Waiter> allWaiters) {
-        dataStorage.allWaiters = allWaiters;
+    public static void setAllWaiters(HashMap<Restaurant, ArrayList<Waiter>> allWaiters) {
+        allWaiters = allWaiters;
     }
 
-    public static HashMap<Restaurant, Chef> getAllChefs() {
+    public static HashMap<Restaurant, ArrayList<Chef>> getAllChefs() {
         return allChefs;
     }
 
-    public static void setAllChefs(HashMap<Restaurant, Chef> allChefs) {
-        dataStorage.allChefs = allChefs;
+    public static void setAllChefs(HashMap<Restaurant, ArrayList<Chef>> allChefs) {
+        allChefs = allChefs;
     }
 
-    public static HashMap<Restaurant, Customer> getAllCustomers() {
+    public static HashMap<Restaurant, ArrayList<Customer>> getAllCustomers() {
         return allCustomers;
     }
 
-    public static void setAllCustomers(HashMap<Restaurant, Customer> allCustomers) {
-        dataStorage.allCustomers = allCustomers;
+    public static void setAllCustomers(HashMap<Restaurant, ArrayList<Customer>> allCustomers) {
+        allCustomers = allCustomers;
     }
 
-    public ArrayList
+    public static ArrayList<Table> getTablesForRestaurant(String restaurantId) {
+        return allTables.get(allRestaurants.get(restaurantId));
+    }
+
+    public static ArrayList<Chef> getChefsForRestaurant(String restaurantId) {
+        return allChefs.get(allRestaurants.get(restaurantId));
+    }
+
+    public static ArrayList<Waiter> getWaitersForRestaurant(String restaurantId) {
+        return allWaiters.get(allRestaurants.get(restaurantId));
+    }
 }
