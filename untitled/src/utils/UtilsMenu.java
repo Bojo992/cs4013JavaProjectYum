5c8fdcf6bd18f9014d9a2ac576b901e0b3a5802c
zed992 <93447305+zed992@users.noreply.github.com>
2022-11-22 16:12:55 +0000

    Fix of choose and show functions in UtilsMenu.

--- untitled/src/utils/UtilsMenu.java
+++ untitled/src/utils/UtilsMenu.java
@@ -1,22 +1,22 @@
-package CLI;
+package utils;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public abstract class UtilsMenu<Object> {
+public abstract class UtilsMenu {
     private Scanner in;
 
     public abstract void run();
 
-    protected Object Choice(ArrayList<Object> choices) {
-        if (choices.isEmpty()) {
+    protected Object chooseFirstType(Object[] choices) {
+        if (choices.length == 0) {
             System.out.println("There are no options chose from");
             return null;
         }
+
         while (true) {
             int i = 0;
 
-            System.out.printf(" %d) Exit", i);
+            System.out.printf(" %d) Exit\n", i);
             for (Object c : choices) {
                 i++;
                 System.out.printf(" %d) %s", i, c.toString());
@@ -36,8 +36,8 @@ public abstract class UtilsMenu<Object> {
                 if (j == 0){
                     return null;
                 }
-                if (j>0 && j <= choices.size()){
-                    return choices.get(j - 1);
+                if (j>0 && j <= choices.length){
+                    return choices[j - 1];
                 }
 
                 System.out.println("Please pick from above");
@@ -45,8 +45,8 @@ public abstract class UtilsMenu<Object> {
         }
     }
 
-    protected void show(ArrayList<Object> choices){
-        int i = 0;
+    protected void showFirstType(Object[] choices){
+        int i = 1;
         for (Object c:choices){
             System.out.printf(" %d) %s", i, c.toString());
         }
