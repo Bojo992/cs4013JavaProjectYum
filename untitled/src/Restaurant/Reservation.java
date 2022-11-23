030de8dcda3f0910df77c3146a320330b0f279af
Craig_ <21340633@studentmail.ul.ie>
2022-11-23 12:34:36 +0000

    Reservation to CSV
    needs to be moved to utils

---
+++ untitled/src/Restaurant/Reservation.java
@@ -0,0 +1,67 @@
+package Restaurant;
+
+import People.Customer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Reservation {
+    private Customer customer;
+    private String date;
+    private Table table;
+    private Restaurant restaurant;
+    private int noOfCustomer;
+
+    public Reservation(Customer customer, String date, Table table, Restaurant restaurant, int noOfCustomer) {
+        this.customer = customer;
+        this.date = date;
+        this.table = table;
+        this.restaurant = restaurant;
+        this.noOfCustomer = noOfCustomer;
+    }
+
+    public String resToCSV(File file) throws IOException {//Todo move to UtilsCSV
+        FileWriter writer;
+        File reservationCSV =new File("reservation.csv");
+        Scanner in = new Scanner(file);
+
+        if (reservationCSV.exists() && reservationCSV.canWrite()) {
+            writer = new FileWriter(reservationCSV, true);
+        } else {
+            writer = new FileWriter(reservationCSV);
+        }
+        StringBuilder resString = new StringBuilder();
+        //resString.append();ToDo use .toCSV
+        return String.valueOf(resString);
+    }
+
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    @Override
+    public String toString() {
+        return "Table number: " + table.getTableNo() + "Time: " + date.toString();
+    }
+
+    public String toCSV(){
+        return  customer.getId() + ", " + date + ", " +
+                table.getTableNo() + ", " + restaurant.getId() + ", " + noOfCustomer;
+    }
+}
