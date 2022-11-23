21a92a0ef5d24ee3ca139e19b0f8413327317981
zed992 <93447305+zed992@users.noreply.github.com>
2022-11-23 11:12:04 +0000

    Write restaurant to csv.

--- untitled/src/Restaurant/UtilsCSV.java
+++ untitled/src/Restaurant/UtilsCSV.java
@@ -1,8 +1,17 @@
 package Restaurant;
 
+import People.Chef;
+import People.Waiter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.WildcardType;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,5 +41,57 @@ public class UtilsCSV {
         double price = Double.parseDouble(metadata[1]);
 
         return new Food(name,price);
+    }
+
+    public void writeRestaurant(Restaurant restaurant) throws IOException {
+        File restaurantsCSV = new File("restaurant.csv");
+        FileWriter writer;
+        Scanner in = new Scanner(restaurantsCSV);
+
+        if (restaurantsCSV.exists() && restaurantsCSV.canWrite()) {
+            writer = new FileWriter(restaurantsCSV, true);
+        } else {
+            writer = new FileWriter(restaurantsCSV);
+        }
+
+        StringBuilder restaurantString = new StringBuilder();
+
+        restaurantString.append(String.format("restaurant, %s, %d\r\n", restaurant.getId(), restaurant.getNumberOfTables()));
+        restaurantString.append("tables, " + "\r\n");
+
+        for (Table table : restaurant.getTables()) {
+            String tableCSV = table.getTableNo() + ", " + table.getSeats() + ", ";
+
+            for (String date : table.getAvailability().keySet()) {
+                restaurantString.append(tableCSV + date + ", " + table.getAvailability().get(date) + "\r\n");
+            }
+        }
+
+        restaurantString.append("waiters\r\n");
+
+        for (Waiter waiter : restaurant.getWaiters()) {
+            restaurantString.append(waiter.toCSV());
+        }
+
+        restaurantString.append("chef\r\n");
+
+        for (Chef chef : restaurant.getChefs()) {
+            restaurantString.append(chef.toCSV());
+        }
+
+        restaurantString.append("menu\r\n");
+
+        for (var category: restaurant.getMenu().getCategories()) {
+            restaurantString.append(category + ", " + category.getName() + "\r\n");
+
+            for (Food food: category.getFood()) {
+                restaurantString.append(food.toCSV());
+            }
+        }
+
+
+        writer.write(restaurantString.toString());
+
+        writer.close();
     }
 }
