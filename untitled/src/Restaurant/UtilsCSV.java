0bd0293bd9a64551944441108fd4a6443745170a
zed992 <93447305+zed992@users.noreply.github.com>
2022-11-23 11:12:34 +0000

    Restaurant reader from csv.

--- untitled/src/Restaurant/UtilsCSV.java
+++ untitled/src/Restaurant/UtilsCSV.java
@@ -94,4 +94,115 @@ public class UtilsCSV {
 
         writer.close();
     }
+
+    public ArrayList<Restaurant> readRestaurants() throws FileNotFoundException {
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+
+        File restaurantsCSV = new File("restaurant.csv");
+
+        if (!restaurantsCSV.exists()) {
+            return null;
+        }
+
+        boolean skip = true;
+
+        Scanner in = new Scanner(restaurantsCSV);
+
+        while (in.hasNextLine()) {
+            String line = in. nextLine();
+            String restaurantId = line.split(", ")[1];
+            int numberOfTables = Integer.parseInt(line.split(", ")[2]);
+
+            Restaurant restaurant = new Restaurant(numberOfTables);
+            restaurant.setId(restaurantId);
+
+            line = in.nextLine();
+            line = in.nextLine();
+
+            while (!line.equals("waiters")) {
+                String[] lineArray = line.split(", ");
+                String tableSignature = lineArray[0] + ", " + lineArray[1];
+
+                HashMap<String, Boolean> reservations = new HashMap<>();
+
+                while (line.contains(tableSignature)) {
+                    lineArray = line.split(", ");
+
+                    reservations.put(lineArray[2], (lineArray[3].equals("true") ? true : false));
+
+                    line = in.nextLine();
+                }
+
+                lineArray = tableSignature.split(", ");
+
+                Table temp = new Table(Integer.parseInt(lineArray[0]), Integer.parseInt(lineArray[1]));
+
+                temp.setAvailability(reservations);
+
+                restaurant.addTable(temp);
+
+                line = in.nextLine();
+            }
+
+            line = in.nextLine();
+
+            while (!line.equals("chef")) {
+                String[] lineArray = line.split(", ");
+
+                var waiter = new Waiter(lineArray[0], lineArray[1], lineArray[2]);
+
+                restaurant.addWaiter(waiter, lineArray[4]);
+
+                waiter.setId(lineArray[3]);
+
+                line = in.nextLine();
+            }
+
+            line = in.nextLine();
+
+            while (!line.equals("menu")) {
+                String[] lineArray = line.split(", ");
+
+                var chef = new Chef(lineArray[0], lineArray[1], lineArray[2]);
+
+                restaurant.addChef(chef, lineArray[4]);
+
+                chef.setId(lineArray[3]);
+
+                line = in.nextLine();
+            }
+
+            if (in.hasNextLine()) {
+                line = in.nextLine();
+            } else {
+                skip = false;
+            }
+
+            Menu menu = new Menu();
+
+            while(in.hasNextLine() && !line.contains("restaurant, ") && skip) {
+                String[] lineArray = line.split(", ");
+                String category = lineArray[1];
+
+                menu.addCategory(category);
+
+                while (!line.contains("category, ")) {
+                    line = in.nextLine();
+                    lineArray = line.split(", ");
+                    Food food = new Food(lineArray[0], Double.parseDouble(lineArray[1]));
+
+                    menu.addFood(food, category);
+                }
+
+                line = in.nextLine();
+            }
+
+            restaurant.setMenu(menu);
+
+            restaurants.add(restaurant);
+        }
+
+
+        return restaurants;
+    }
 }
