e383898bedf354c64b1151e1356054794887ab0f
Italo <italoterto4@gmail.com>
2022-11-23 13:47:13 +0000

    bill() method for the order

--- untitled/src/Restaurant/Order.java
+++ untitled/src/Restaurant/Order.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 
 public class Order {
     //ToDo return total price of food
-    // loop thru the arraylist total += food.getPrice()
+    // loop thru the arraylist, total += food.getPrice()
 
     //ToDo add items one by one
     private int id;
+    private ArrayList<Food>[] food;
     private ArrayList<Food> tableOrder;
 
     public Order(Table table) {
         this.tableOrder = new ArrayList<>();
         this.id = table.getTableNo();
+    }
+
+    public int bill() {
+        int total = 0;
+
+        for (int i = 0; i < tableOrder.size(); i++) {
+            total += tableOrder.get(i).getPrice();
+        }
+
+        return total;
     }
 
     public ArrayList<Food> getTableOrder() {
