ee594ae8c65ac72df4a8e751481f6a14e416d579
Italo <italoterto4@gmail.com>
2022-11-23 13:12:36 +0000

    sample order class

--- untitled/src/Restaurant/Order.java
+++ untitled/src/Restaurant/Order.java
@@ -3,10 +3,16 @@ package Restaurant;
 import java.util.ArrayList;
 
 public class Order {
+    //ToDo return total price of food
+    // loop thru the arraylist total += food.getPrice()
+
+    //ToDo add items one by one
+    private int id;
     private ArrayList<Food> tableOrder;
 
-    public Order() {
+    public Order(Table table) {
         this.tableOrder = new ArrayList<>();
+        this.id = table.getTableNo();
     }
 
     public ArrayList<Food> getTableOrder() {
