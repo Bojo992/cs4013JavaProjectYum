3f5ebfce72f01ec443e54a5a2b7b6a20c2f89f89
Simon <102541413+s1m0nWasTaken@users.noreply.github.com>
2022-11-24 00:41:37 +0000

    Main Menu GUI
    Created a menu in GUI

---
+++ untitled/src/Gui/MenuGUI.java
@@ -0,0 +1,94 @@
+/**
+ * The ActionListener Loop here is meant to throw LoginPage, however since we have not implemented it yet, I have
+ * made it skip the login and go straight to selected interface.
+ */
+
+package Gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuGUI implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton buttonEmployee = new JButton("Employee");
+    JButton buttonUser = new JButton("User");
+    JButton buttonChef = new JButton("Chef");
+    JLabel introMessage = new JLabel("Please pick who you want to login as : ");
+    JPanel panel = new JPanel();
+    JPanel introMessagePanel = new JPanel();
+    JPanel back = new JPanel();
+    JButton leave = new JButton("Quit");
+
+    public MenuGUI() {
+
+        frame.add(back, BorderLayout.SOUTH);
+        back.add(leave);
+        leave.addActionListener(this);
+        back.setPreferredSize(new Dimension(400,40));
+        back.setBackground(Color.DARK_GRAY);
+
+        buttonEmployee.setBounds(150, 200, 200, 25);
+        buttonEmployee.addActionListener(this);
+        buttonUser.setBounds(275, 200, 200, 25);
+        buttonUser.addActionListener(this);
+        buttonChef.setBounds(400, 200, 200, 25);
+        buttonChef.addActionListener(this);
+
+        panel.setPreferredSize(new Dimension(500, 500));
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setSize(400, 400);
+        panel.add(buttonEmployee);
+        panel.add(buttonUser);
+        panel.add(buttonChef);
+
+        introMessagePanel.setPreferredSize(new Dimension(400, 60));
+        introMessagePanel.setBackground(Color.GRAY);
+        introMessagePanel.add(introMessage);
+
+        center(frame);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(introMessagePanel, BorderLayout.NORTH);
+        frame.setSize(420, 420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("LoginSelect - Restaurant Management System");
+        frame.setVisible(true);
+    }
+
+    private static void center(JFrame frame) {
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int w = frame.getSize().width;
+        int h = frame.getSize().height;
+        int x = (dim.width - w) / 2;
+        int y = (dim.height - h) / 2;
+
+        frame.setLocation(x,y);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == buttonEmployee) {
+            frame.dispose();
+            EmployeeMenuGUI employeeMenuGUI = new EmployeeMenuGUI();
+        }
+
+        if (e.getSource() == buttonUser) {
+            frame.dispose();
+            UserGUI userGUI = new UserGUI();
+        }
+
+        if (e.getSource() == buttonChef) {
+            frame.dispose();
+            ChefMenuGUI chefMenuGUI = new ChefMenuGUI();
+        }
+
+        if(e.getSource() == leave) {
+            frame.dispose();
+        }
+    }
+}
+
+
