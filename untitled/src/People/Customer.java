f20d3b725ac768da2bf39b82821d382ab5fe3e79
Craig_ <21340633@studentmail.ul.ie>
2022-11-24 10:40:47 +0000

    Javadoc for Customer class

--- untitled/src/People/Customer.java
+++ untitled/src/People/Customer.java
@@ -6,31 +6,53 @@ import Restaurant.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Customer class which extends Person class
+ */
 public class Customer extends Person {
+
     private ArrayList<Reservation> reservations;
 
+    /**
+     * Constructor which specifies name
+     * @param name
+     */
     public Customer(String name) {
         super(name);
         reservations = new ArrayList<>();
     }
 
+    /**
+     * Constructor which specifies name and phone
+     * @param name
+     * @param phone
+     */
     public Customer(String name, String phone) {
         super(name, phone);
         reservations = new ArrayList<>();
     }
 
+    /**
+     * @param reservation
+     * Adds a Reservation object to an ArrayList of reservations
+     */
     public void addReservation(Reservation reservation){
         reservations.add(reservation);
     }
 
+    /**
+     * @param reservation
+     * Removes a Reservation object to an ArrayList of reservations
+     */
     public void removeReservation(Reservation reservation) {
         reservations.remove(reservation);
     }
 
-    /*------------------
-       Getters/Setters
-      ------------------*/
 
+    /**
+     * @return
+     * Gets the ArrayList of reservations
+     */
     public ArrayList<Reservation> getReservations() {
          return reservations;
     }
